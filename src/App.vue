53c1123aba35ee18b30160036f0fa8c8d44a3178
kawla95 <kawlamansur@Kawla-Mansurs-MacBook-Pro.local>
2023-04-04 16:51:17 +0100

    added css

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,58 @@
+<!-- each vue component must at least have a template -->
+<!-- you can have as many root elements in your templates as you wish -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>{{ title }}</h1>
+<!-- wrap div around your prop when you want to include conditional rendering -->
+<div to=".modals" v-if="showModal">
+  <!-- slots can't have a self closing tag  -->
+  <Modal theme="" @close="toggleModal">
+    <!-- to pass a named slot, wrap it in a template so it doesn't run in the default slot -->
+    <template v-slot:links>
+      <a href="#"> sign up now</a>
+      <a href="#"> more info </a>
+    </template>
+   <h1>Ninja Giveaway!</h1> 
+    <p>Grab your ninja swag for half price!</p>
+
+   </Modal>
+</div>
+<div to=".modals" v-if="showModalTwo">
+  <!-- slots can't have a self closing tag  -->
+  <Modal @close="toggleModalTwo">
+    <!-- to pass a named slot, wrap it in a template so it doesn't run in the default slot -->
+   <h1> Sign up to the newsletter</h1> 
+    <p>For updates and promo codes</p>
+   </Modal>
+</div>
+<button @click="toggleModal"> open Modal </button>
+<button @click="toggleModalTwo"> open Modal 2 </button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from './components/Modal'
+//The object below represents root components 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data() {
+    return { 
+      title: "My first Vue App",
+      showModal: false, 
+      showModalTwo: false, 
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    }
   }
 }
 </script>
@@ -22,5 +65,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+//here is where we render vue components to the DOM
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/global.css'
 
 createApp(App).mount('#app')
